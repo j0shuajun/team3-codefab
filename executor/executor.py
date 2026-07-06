2de795d4abfa53bd86a7210f06da7b15c038bfce
soyeongk <soyeong_k@naver.com>
2026-07-07 08:26:58 +0900

    refactor: evaluate 내에 binary 연산 분리

--- executor/executor.py
+++ executor/executor.py
@@ -28,29 +28,27 @@ class Executor:
             return expr.value
 
         if isinstance(expr, BinaryExpr):
-            left = self.evaluate(expr.left)
-            right = self.evaluate(expr.right)
-
-            if expr.operator.type == TokenType.PLUS:
-                return left + right
-
-        if isinstance(expr, BinaryExpr):
-            left = self.evaluate(expr.left)
-            right = self.evaluate(expr.right)
-
-            if expr.operator.type == TokenType.PLUS:
-                return left + right
-
-            if expr.operator.type == TokenType.MINUS:
-                return left - right
-
-            if expr.operator.type == TokenType.STAR:
-                return left * right
-
-            if expr.operator.type == TokenType.SLASH:
-                return left / right
+            return self.evaluate_binary(expr)
 
         raise RuntimeError(f"Unknown expression type: {type(expr).__name__}")
+
+    def evaluate_binary(self, expr):
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        if expr.operator.type == TokenType.PLUS:
+            return left + right
+
+        if expr.operator.type == TokenType.MINUS:
+            return left - right
+
+        if expr.operator.type == TokenType.STAR:
+            return left * right
+
+        if expr.operator.type == TokenType.SLASH:
+            return left / right
+
+        raise RuntimeError(f"Unknown binary operator: {expr.operator.origin}")
 
     def stringify(self, value):
         return str(value)
