cfc6a02a6fbca985d846d7cd5f65c49e27fcf194
Jinwoo Park <46846964+Jinops@users.noreply.github.com>
2026-07-06 14:35:53 +0900

    [feature] Token, Token Type 스켈레톤 추가

---
+++ Token.py
@@ -0,0 +1,9 @@
+class TokenType(Enum):
+    IF = auto()
+    FOR = auto()
+    VAR = auto()
+
+class Token:
+    def __init__(self, type: TokenType, origin: str):
+        self.type = type
+        self.origin = origin
