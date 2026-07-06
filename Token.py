8c61fe6aab74d60013906810635f9cd9b52f997b
Jinwoo Park <46846964+Jinops@users.noreply.github.com>
2026-07-06 14:44:03 +0900

    [feature] TokenType 추가

--- Token.py
+++ Token.py
@@ -1,7 +1,39 @@
 class TokenType(Enum):
-    IF = auto()
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    SEMICOLON = auto()
+
+    PLUS = auto()
+    MINUS = auto()
+    STAR = auto()
+    SLASH = auto()
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    AND = auto()
+    ELSE = auto()
+    FALSE = auto()
     FOR = auto()
+    IF = auto()
+    OR = auto()
+    PRINT = auto()
+    TRUE = auto()
     VAR = auto()
+
+    EOF = auto()
 
 class Token:
     def __init__(self, type: TokenType, origin: str):
