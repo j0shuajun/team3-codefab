070aa92b3c89df6af647b9b50e0d6622364d1b32
yejinlee <progress_6@naver.com>
2026-07-06 16:07:58 +0900

    [test] checker testcase 추가 - 같은 스코프 내 변수 중복 선언 오류 검출

---
+++ tests/test_checker.py
@@ -0,0 +1,59 @@
+from checker import Checker
+from expr import LiteralExpr
+from statement import BlockStmt, VarStmt
+
+
+def check(statements):
+    return Checker().check(statements)
+
+
+class TestDuplicateDeclaration:
+    def test_duplicate_declaration_in_global_scope_is_error(self):
+        statements = [
+            VarStmt("a", LiteralExpr(1)),
+            VarStmt("a", LiteralExpr(2)),
+        ]
+
+        errors = check(statements)
+
+        assert len(errors) == 1
+        assert "a" in errors[0]
+
+    def test_unique_declarations_produce_no_error(self):
+        statements = [
+            VarStmt("a", LiteralExpr(1)),
+            VarStmt("b", LiteralExpr(2)),
+        ]
+
+        assert check(statements) == []
+
+    def test_duplicate_declaration_inside_same_block_is_error(self):
+        statements = [
+            BlockStmt([
+                VarStmt("x", LiteralExpr(1)),
+                VarStmt("x", LiteralExpr(2)),
+            ]),
+        ]
+
+        errors = check(statements)
+
+        assert len(errors) == 1
+        assert "x" in errors[0]
+
+    def test_shadowing_in_nested_block_is_allowed(self):
+        statements = [
+            VarStmt("x", LiteralExpr(1)),
+            BlockStmt([
+                VarStmt("x", LiteralExpr(2)),
+            ]),
+        ]
+
+        assert check(statements) == []
+
+    def test_sibling_blocks_may_reuse_same_name(self):
+        statements = [
+            BlockStmt([VarStmt("x", LiteralExpr(1))]),
+            BlockStmt([VarStmt("x", LiteralExpr(2))]),
+        ]
+
+        assert check(statements) == []
