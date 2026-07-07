8fe0a9858a0b3b63240b470c31e88313dbeb80fe
yejinlee <progress_6@naver.com>
2026-07-07 12:38:43 +0900

    [refactor] resolvers method 추가, CheckerError 추가

--- checker/checker.py
+++ checker/checker.py
@@ -10,10 +10,30 @@ from assembler.expr import (
 from assembler.statement import BlockStmt, ExpressionStmt, ForStmt, IfStmt, VarStmt
 
 
+class CheckerError(Exception):
+    """Checker 가 처리할 수 없는 노드를 만났을 때 발생하는 예외."""
+
+
 class Checker:
     def __init__(self):
         self.errors = []
         self.scopes = []
+        self._statement_resolvers = {
+            VarStmt: self._resolve_var_stmt,
+            BlockStmt: self._resolve_block_stmt,
+            ExpressionStmt: self._resolve_expression_stmt,
+            IfStmt: self._resolve_if_stmt,
+            ForStmt: self._resolve_for_stmt,
+        }
+        self._expr_resolvers = {
+            VariableExpr: self._resolve_variable_expr,
+            AssignExpr: self._resolve_assign_expr,
+            BinaryExpr: self._resolve_branching_expr,
+            LogicalExpr: self._resolve_branching_expr,
+            UnaryExpr: self._resolve_unary_expr,
+            GroupingExpr: self._resolve_grouping_expr,
+            LiteralExpr: self._resolve_literal_expr,
+        }
 
     def check(self, statements):
         self.errors = []
@@ -26,24 +46,25 @@ class Checker:
             self._resolve_statement(statement)
 
     def _resolve_statement(self, statement):
-        if isinstance(statement, VarStmt):
-            self._resolve_var_stmt(statement)
-        elif isinstance(statement, BlockStmt):
-            self._resolve_block_stmt(statement)
-        elif isinstance(statement, ExpressionStmt):
-            self._resolve_expr(statement.expression)
-        elif isinstance(statement, IfStmt):
-            self._resolve_if_stmt(statement)
-        elif isinstance(statement, ForStmt):
-            self._resolve_for_stmt(statement)
+        resolver = self._statement_resolvers.get(type(statement))
+        if resolver is None:
+            raise CheckerError(f"Unknown statement type: {type(statement).__name__}")
+        resolver(statement)
+
+    def _resolve_expr(self, expr):
+        resolver = self._expr_resolvers.get(type(expr))
+        if resolver is None:
+            raise CheckerError(f"Unknown expression type: {type(expr).__name__}")
+        resolver(expr)
+
+    # --- statement resolvers -------------------------------------------------
 
     def _resolve_var_stmt(self, statement):
         name = statement.name.origin
         scope = self.scopes[-1]
+
         if name in scope:
-            self.errors.append(
-                f"Variable '{name}' already declared in this scope."
-            )
+            self.errors.append(f"Variable '{name}' already declared in this scope.")
         else:
             scope[name] = False
 
@@ -53,8 +74,13 @@ class Checker:
 
     def _resolve_block_stmt(self, statement):
         self.scopes.append({})
-        self._resolve_statements(statement.statements)
-        self.scopes.pop()
+        try:
+            self._resolve_statements(statement.statements)
+        finally:
+            self.scopes.pop()
+
+    def _resolve_expression_stmt(self, statement):
+        self._resolve_expr(statement.expression)
 
     def _resolve_if_stmt(self, statement):
         self._resolve_expr(statement.condition)
@@ -71,38 +97,38 @@ class Checker:
             self._resolve_expr(statement.increment)
         self._resolve_statement(statement.body)
 
-    def _resolve_expr(self, expr):
-        if isinstance(expr, VariableExpr):
-            self._resolve_variable_expr(expr)
-        elif isinstance(expr, AssignExpr):
-            self._resolve_assign_expr(expr)
-        elif isinstance(expr, BinaryExpr):
-            self._resolve_expr(expr.left)
-            self._resolve_expr(expr.right)
-        elif isinstance(expr, LogicalExpr):
-            self._resolve_expr(expr.left)
-            self._resolve_expr(expr.right)
-        elif isinstance(expr, UnaryExpr):
-            self._resolve_expr(expr.right)
-        elif isinstance(expr, GroupingExpr):
-            self._resolve_expr(expr.expression)
-        elif isinstance(expr, LiteralExpr):
-            pass
+    # --- expression resolvers -------------------------------------------------
+
+    def _resolve_branching_expr(self, expr):
+        self._resolve_expr(expr.left)
+        self._resolve_expr(expr.right)
+
+    def _resolve_unary_expr(self, expr):
+        self._resolve_expr(expr.right)
+
+    def _resolve_grouping_expr(self, expr):
+        self._resolve_expr(expr.expression)
+
+    def _resolve_literal_expr(self, expr):
+        pass
 
     def _resolve_variable_expr(self, expr):
         name = expr.name.origin
-        for scope in reversed(self.scopes):
-            if name in scope:
-                if scope[name] is False:
-                    self.errors.append(
-                        f"Variable '{name}' is used before initialization."
-                    )
-                return
+        scope = self._find_declaring_scope(name)
+        if scope is not None and scope[name] is False:
+            self.errors.append(f"Variable '{name}' is used before initialization.")
 
     def _resolve_assign_expr(self, expr):
         self._resolve_expr(expr.value)
         name = expr.name.origin
+        scope = self._find_declaring_scope(name)
+        if scope is not None:
+            scope[name] = True
+
+    # --- helpers -------------------------------------------------
+
+    def _find_declaring_scope(self, name):
         for scope in reversed(self.scopes):
             if name in scope:
-                scope[name] = True
-                return
+                return scope
+        return None
