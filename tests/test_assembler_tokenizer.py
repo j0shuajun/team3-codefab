ef3b8d5f6b002475806843f3402b59804323e9a3
Jinwoo Park <46846964+Jinops@users.noreply.github.com>
2026-07-07 09:37:06 +0900

    [style] formatting

--- tests/test_assembler_tokenizer.py
+++ tests/test_assembler_tokenizer.py
@@ -28,6 +28,7 @@ def test_if_statement(tokenizer):
                       Token(T.RIGHT_PAREN, ")"),
                       Token(T.EOF, "")]
 
+
 def test_plus_operator(tokenizer):
     tokens = tokenizer.tokenize("a + b")
 
@@ -36,6 +37,7 @@ def test_plus_operator(tokenizer):
                       Token(T.IDENTIFIER, "b"),
                       Token(T.EOF, "")]
 
+
 def test_multiply_operator(tokenizer):
     tokens = tokenizer.tokenize("b * 3")
 
@@ -43,6 +45,7 @@ def test_multiply_operator(tokenizer):
                       Token(T.STAR, "*"),
                       Token(T.NUMBER, "3", value=3.0),
                       Token(T.EOF, "")]
+
 
 def test_plus_multiply_operator(tokenizer):
     tokens = tokenizer.tokenize("a + b * 3")
@@ -54,6 +57,7 @@ def test_plus_multiply_operator(tokenizer):
                       Token(T.NUMBER, "3", value=3.0),
                       Token(T.EOF, "")]
 
+
 def test_minus_operator(tokenizer):
     tokens = tokenizer.tokenize("a - b")
 
@@ -62,6 +66,7 @@ def test_minus_operator(tokenizer):
                       Token(T.IDENTIFIER, "b"),
                       Token(T.EOF, "")]
 
+
 def test_divide_operator(tokenizer):
     tokens = tokenizer.tokenize("10 / 2")
 
@@ -69,6 +74,7 @@ def test_divide_operator(tokenizer):
                       Token(T.SLASH, "/"),
                       Token(T.NUMBER, "2", value=2.0),
                       Token(T.EOF, "")]
+
 
 def test_grouping(tokenizer):
     tokens = tokenizer.tokenize("( a + b )")
@@ -80,6 +86,7 @@ def test_grouping(tokenizer):
                       Token(T.RIGHT_PAREN, ")"),
                       Token(T.EOF, "")]
 
+
 def test_block_scope(tokenizer):
     tokens = tokenizer.tokenize("{ a + b }")
 
@@ -89,6 +96,7 @@ def test_block_scope(tokenizer):
                       Token(T.IDENTIFIER, "b"),
                       Token(T.RIGHT_BRACE, "}"),
                       Token(T.EOF, "")]
+
 
 def test_semicolon(tokenizer):
     tokens = tokenizer.tokenize("a=3;b=4")
@@ -102,6 +110,7 @@ def test_semicolon(tokenizer):
                       Token(T.NUMBER, "4", value=4.0),
                       Token(T.EOF, "")]
 
+
 def test_less_than(tokenizer):
     tokens = tokenizer.tokenize("a < 3")
 
@@ -109,6 +118,7 @@ def test_less_than(tokenizer):
                       Token(T.LESS, "<"),
                       Token(T.NUMBER, "3", value=3.0),
                       Token(T.EOF, "")]
+
 
 def test_greater_than(tokenizer):
     tokens = tokenizer.tokenize("a > 3")
@@ -118,6 +128,7 @@ def test_greater_than(tokenizer):
                       Token(T.NUMBER, "3", value=3.0),
                       Token(T.EOF, "")]
 
+
 def test_comma(tokenizer):
     tokens = tokenizer.tokenize("a, b")
 
@@ -125,6 +136,7 @@ def test_comma(tokenizer):
                       Token(T.COMMA, ","),
                       Token(T.IDENTIFIER, "b"),
                       Token(T.EOF, "")]
+
 
 def test_assign_with_comma(tokenizer):
     tokens = tokenizer.tokenize("a, b = 3, 4")
@@ -137,6 +149,7 @@ def test_assign_with_comma(tokenizer):
                       Token(T.COMMA, ","),
                       Token(T.NUMBER, "4", value=4.0),
                       Token(T.EOF, "")]
+
 
 def test_bang(tokenizer):
     tokens = tokenizer.tokenize("!a")
