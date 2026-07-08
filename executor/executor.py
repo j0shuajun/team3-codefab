93b5884610e4d0b7b23628a641ff6a75321b94b7
soyeongk <soyeong_k@naver.com>
2026-07-08 10:54:20 +0900

    feature: extract environment and add user function

--- executor/executor.py
+++ executor/executor.py
@@ -18,43 +18,11 @@ from assembler.statement import (
     VarStmt,
 )
 from assembler.tokenizer import TokenType
+from assembler.environment import Environment
 
 
 class CodeFabRuntimeError(Exception):
     pass
-
-
-class Environment:
-    def __init__(self, enclosing=None):
-        self.values = {}
-        self.enclosing = enclosing
-
-    def define(self, name, value):
-        self.values[name] = value
-
-    def get(self, name_token):
-        name = name_token.origin
-
-        if name in self.values:
-            return self.values[name]
-
-        if self.enclosing is not None:
-            return self.enclosing.get(name_token)
-
-        raise CodeFabRuntimeError(f"Undefined variable '{name}'.")
-
-    def assign(self, name_token, value):
-        name = name_token.origin
-
-        if name in self.values:
-            self.values[name] = value
-            return
-
-        if self.enclosing is not None:
-            self.enclosing.assign(name_token, value)
-            return
-
-        raise CodeFabRuntimeError(f"Undefined variable '{name}'.")
 
 
 class Executor:
